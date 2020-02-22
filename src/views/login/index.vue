a494c06d91e1954541f7d5a6057e549310d85d2f
zijie <897872164@qq.com>
2020-02-22 11:10:06 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    正式开始
+    <el-button>默认按钮</el-button>
+    <el-button type="danger" round>危险按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+#login {
+  background-color: #344a5f;
+  height: 100vh;
+}
+</style>
